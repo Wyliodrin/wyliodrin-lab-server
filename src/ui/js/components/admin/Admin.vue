6daa7c893bd9142062a13562746555418267cf29
Ovidiu Stoica <oviska@gmail.com>
2018-09-05 13:26:51 +0300

    formatting and css styling

--- src/ui/js/components/admin/Admin.vue
+++ src/ui/js/components/admin/Admin.vue
@@ -1,81 +1,64 @@
 <template>
-	<div class="content w-100">
-		<nav class="navbar navbar-expand-lg navbar-inverse navbar-static-top p-0 w-100 wsd-menu" id="slide-nav">
+
+	<div v-if="user === null">
+		Loading
+	</div>
+	<div class="w-100" v-else>
+		<nav class="navbar navbar-expand-lg navbar-inverse navbar-static-top p-0 w-100 welcome-menu" id="slide-nav">
 			<a class="navbar-brand pt-0 pb-0 pl-4" href="index.html"><img src="img/logo.png"></a>
 			<button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<img src="img/icons/menu-icon.png">
 			</button>
 			<div class="navbar-toggler hidden-sm-up close-area collapsed" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></div>
 			<div class="collapse navbar-collapse no-transition navbar-toggleable-xs" id="navbarResponsive">
-				<!-- <div class="nav navbar-nav nav-switch mr-auto" >
-					<div class="options-list">
-						<a href="/studio.html" data-toggle="tooltip" target="_blank" v-tooltip data-placement="bottom" title="Develop Applications using the Studio" class="switch-app">
-							<img src="img/icon-editor.png">
-							<span>STUDIO</span>
-						</a>
-					</div>
-				</div> -->
+				<div class="nav navbar-nav nav-switch mr-auto" ></div>
 				<div class="nav navbar-nav nav-btns" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false">
 					<div style="float: left">
-						<!--<div class="run-proj right">
-							<a href="#" data-toggle="modal" data-target="#myRunModal">
-								<img src="img/run-project.png">
-								<span> Run project</span>
-								<div class="triangle"></div>
-							</a>
-						</div>-->
-						<div class="main-btn left">
+						<div class="my-boards left">
 							<router-link to="/" :class="{'active':activeDashboard}" exact>
-								<i class="i-dashboard nav-icon"></i>
+								<img src="img/dashboard.png">
 								<span> Dashboard</span>
 								<div class="triangle"></div>
 							</router-link>
 						</div>
-						<div class="main-btn left">
+						<div class="my-boards left">
 							<router-link to="/users" :class="{'active':activeUsers}">
-								<i class="i-device nav-icon"></i>
+								<img src="img/users.png">
 								<span> Users</span>
 								<div class="triangle"></div>
 							</router-link>
 						</div>
-						<div class="main-btn left">
-							<router-link to="/courses" class="menu-last-dist" :class="{'active':activeCourses}">
-								<i class="i-product nav-icon"></i>
+						<div class="my-boards left">
+							<router-link to="/courses" :class="{'active':activeCourses}">
+								<img src="img/courses.png">
 								<span> Courses</span>
 								<div class="triangle"></div>
 							</router-link>
 						</div>
-						<div class="main-btn left">
-							<router-link to="/raspberrypi" class="menu-last-dist" :class="{'active':activeRaspberryPi}">
-								<i class="i-product nav-icon"></i>
+						<div class="my-boards left">
+							<router-link to="/raspberrypi" :class="{'active':activeRaspberryPi}">
+								<img src="img/raspberrypi.png">
 								<span> Raspberry Pi</span>
 								<div class="triangle"></div>
 							</router-link>
 						</div>
-						<div class="main-btn left">
+						<div class="my-boards left">
 							<router-link href="#" to="/boards" class="menu-last-dist" :class="{'active':activeBoards}">
-								<i class="i-product nav-icon"></i>
+								<img src="img/my-boards.png">
 								<span> Boards</span>
 								<div class="triangle"></div>
 							</router-link>
 						</div>
-						<!-- <div class="main-btn left">
-							<router-link href="#" to="/settings" class="menu-last-dist">
-								<i class="i-account nav-icon"></i>
-								<span> Settings</span>
-								<div class="triangle"></div>
-							</router-link>
-						</div> -->
 					</div>
 					<div class="user right connected" style="float: right">
-						<a href="#">{{user.name}}</a><span class="user-image">
+						<a href="#">{{user.name}}Username</a><span class="user-image">
 						<img :src="gravatar"></span>
 						<div class="triangle"></div>
 						<div class="options-list">
 							<!--<a href="#" data-toggle="tooltip" data-placement="bottom" title="Tutorials"><img src="img/icon-tutorial.png"></a>
 							<a href="#" data-toggle="tooltip" data-placement="bottom" title="Take the tour"><img src="img/icon-tour.png"></a>
-							<a href="#" data-toggle="tooltip" data-placement="bottom" v-tooltip title="Notifications"><img src="img/icon-notification.png">  </a>-->
-							<!-- <a href="/docs" data-toggle="tooltip" data-placement="bottom" v-tooltip title="Documentation" target="_blank" class="doc-link"><img src="img/icon-tutorial.png"></a> -->
+							<a href="#" data-toggle="tooltip" data-placement="bottom" v-tooltip title="Notifications"><img src="img/icon-notification.png">  </a>
+							<a href="/docs" data-toggle="tooltip" data-placement="bottom" v-tooltip title="Documentation" target="_blank" class="doc-link"><img src="img/icon-tutorial.png"></a> -->
 							<a data-toggle="modal" data-placement="bottom" title="Settings" v-tooltip data-target="#settingsModal" class="settings-link"><img src="img/icon-settings.png"></a>
 							<a data-toggle="tooltip" data-placement="bottom" v-tooltip title="Logout" class="logout-link" @click="logout"><img src="img/icon-logoff.png"></a>
 						</div>
@@ -83,14 +66,14 @@
 				</div>
 			</div>
 		</nav>
+
 		<div class="admin-page">
 			<!-- <p> Bun venit in pagina de admin </p> -->
 
 			<router-view></router-view>
-
-			
 		</div>
 	</div>
+
 </template>
 
 <script>
--- src/ui/js/components/login/Login.vue
+++ src/ui/js/components/login/Login.vue
@@ -1,24 +1,32 @@
 <template>
-	<div class="login-box d-flex align-items-center">
-		<!-- <form action="" method="post" name="Login_Form" class="form-signin"> -->
-		<div class="form-signin">
-			<center><img src="/img/logo.png" class="m-3"></center>
-			<hr class="colorgraph">
-			<h5 class="form-signin-heading">Welcome! Please sign in</h5>
-			<br>
-			<div v-if="working" class="d-flex justify-content-center align-items-center">
-				<img src="/img/loading-white.gif">
-			</div>
-			<div v-else>
-				<input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" @keyup.enter="login" v-model="username"/>
-				<input type="password" class="form-control" name="Password" placeholder="Password" required="" @keyup.enter="login" v-model="password"/>
-				<button class="btn btn-login btn-block" name="Submit" value="Login" @click="login">Login</button>
-				<!-- <button class="btn btn-signup btn-block" value="Create account" @click="createAccount">Create account</button> -->
-				<!-- <button class="btn btn-signup btn-block" value="Create account" @click="requestDemo">Request demo account</button> -->
-				<a href="#" class="recoverpass" @click="recoverPassword">Forgot password</a>
-			</div>
+	<div>
+		<div class="courseinfo">
+		<h1><img src="/img/idea.png"> Welcome to the Intermediate IoT Course</h1>
+		<span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+		<span>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</span>
 		</div>
-		<!-- </form> -->
+		<div class="login-box d-flex align-items-center">
+			<div class="triangle"></div>
+			<!-- <form action="" method="post" name="Login_Form" class="form-signin"> -->
+			<div class="form-signin">
+				<center><img src="/img/logo.png" class="m-3"></center>
+				<hr class="colorgraph">
+				<h5 class="form-signin-heading">Welcome! Please sign in</h5>
+				<br>
+				<div v-if="working" class="d-flex justify-content-center align-items-center">
+					<img src="/img/loading-white.gif">
+				</div>
+				<div v-else>
+					<input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" @keyup.enter="login" v-model="username"/>
+					<input type="password" class="form-control" name="Password" placeholder="Password" required="" @keyup.enter="login" v-model="password"/>
+					<button class="btn btn-login btn-block" name="Submit" value="Login" @click="login">Login</button>
+					<!-- <button class="btn btn-signup btn-block" value="Create account" @click="createAccount">Create account</button> -->
+					<!-- <button class="btn btn-signup btn-block" value="Create account" @click="requestDemo">Request demo account</button> -->
+					<a href="#" class="recoverpass" @click="recoverPassword">Forgot password</a>
+				</div>
+			</div>
+			<!-- </form> -->
+		</div>
 	</div>
 </template>
 
