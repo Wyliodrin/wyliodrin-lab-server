46fd5e3d158b6f0a4f57b7de72211324c3819432
raz <serban.razvan.96@gmail.com>
2018-09-17 14:19:44 +0300

    file added command not found

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-set -e
 
 ACTUALIP="$(hostname -I | cut -d " " -f 1)"
 
-sudo sed -i -e 's/PLACEHOLDER/'"$ACTUALIP"'/g' /etc/dnsmasq.conf
+sed -i -e 's/PLACEHOLDER/'"$ACTUALIP"'/g' /etc/dnsmasq.conf
 
-node /wyliodrin-lab-server/build/server/server.js
+
+while [ 1 ]
+do
+	tail -f /dev/null
+	echo "tail done"
+done
+
+cd wyliodrin-lab-server/build/server && node server.js
